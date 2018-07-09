cef1b68dbfde99647e87acd0ece745a26a9dfe9f
LuckySpirit <lucspirit@mail.ru>
2018-07-09 11:37:11 +0300

    1545

--- send.php
+++ send.php
@@ -1,20 +1,27 @@
-<?
-if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")&&(isset($_POST['comment'])&&$_POST['comment']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
-    $to = 'ilucspirit@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
-    $subject = 'Обратный звонок'; //Загаловок сообщения
-    $message = '
-                <html>
-                    <head>
-                        <title>'.$subject.'</title>
-                    </head>
-                    <body>
-                        <p>Имя: '.$_POST['name'].'</p>
-                        <p>Телефон: '.$_POST['phone'].'</p>
-                        <p>Комментарий: '.$_POST['comment'].'</p>                        
-                    </body>
-                </html>'; //Текст нащего сообщения можно использовать HTML теги
-    $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
-    $headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
-    mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+<?php
+
+if(isset($_POST['submit'])){
+    $to = "ilucspirit@gmail.com"; // Здесь нужно написать e-mail, куда будут приходить письма
+    $from = "no-reply@zodiac-studio.ru"; // Здесь нужно написать e-mail, от кого будут приходить письма, например no-reply@epicblog.net
+    $name = $_POST['name'];
+    $subject = "Форма отправки сообщений с сайта";
+    $subject2 = "Copy of your form submission";
+    $message = "ФИО посетителя: ". $name . " | Адрес электронной почты: "  . $_POST['email'] . " | Телефон: "  . $_POST['phone'] . "| Комментарий: " . $_POST['comment'];
+    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+
+    mail($to,$subject,$message,$headers);
+    // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Отключено!
+    echo "Сообщение отправлено. Спасибо Вам " . $name . ", мы скоро свяжемся с Вами.";
+    echo "<br /><br /><a href='https://zodiac-studio.ru'>Вернуться на сайт.</a>";
+
 }
+
 ?>
+<!--Переадресация на главную страницу сайта, через 3 секунды-->
+<script language="JavaScript" type="text/javascript">
+    function changeurl(){eval(self.location="https://epicblog.net/index.php");}
+    window.setTimeout("changeurl();",3000);
+</script>
